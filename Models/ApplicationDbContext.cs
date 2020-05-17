db9b4d997dcd6a7dd7bf785f66133245e7167f1e
alex <839928622@qq.com>
2020-05-17 12:59:41 +0800

    DistributionInvoiceConfiguration

--- Models/ApplicationDbContext.cs
+++ Models/ApplicationDbContext.cs
@@ -559,7 +559,7 @@ namespace ERP_API.Models
             modelBuilder.ApplyConfiguration(new DistributionCustomerInvoiceDetailConfiguration());
             modelBuilder.ApplyConfiguration(new DistributionCustomerReceiveMoneyConfiguration());
             modelBuilder.ApplyConfiguration(new DistributionCustomerReceiveMoneyDetailConfiguration());
-
+            modelBuilder.ApplyConfiguration(new DistributionInvoiceConfiguration());
 
 
 
@@ -582,135 +582,7 @@ namespace ERP_API.Models
 
            
 
-            modelBuilder.Entity<DistributionInvoice>(entity =>
-            {
-                entity.Property(e => e.BranchId).HasComment("开票方，即销方");
-
-                entity.Property(e => e.BuyerAddress)
-                    .HasMaxLength(80)
-                    .IsUnicode(false)
-                    .HasComment("购方地址");
-
-                entity.Property(e => e.BuyerBank)
-                    .HasMaxLength(200)
-                    .IsUnicode(false)
-                    .HasComment("购方开户行");
-
-                entity.Property(e => e.BuyerBankAccount)
-                    .HasMaxLength(100)
-                    .IsUnicode(false)
-                    .HasComment("购方开户行账号");
-
-                entity.Property(e => e.BuyerName)
-                    .IsRequired()
-                    .HasMaxLength(200)
-                    .IsUnicode(false)
-                    .HasComment("购方名称");
-
-                entity.Property(e => e.BuyerPhone)
-                    .HasMaxLength(20)
-                    .IsUnicode(false)
-                    .HasComment("购方电话");
-
-                entity.Property(e => e.BuyerTaxNo)
-                    .IsRequired()
-                    .HasMaxLength(20)
-                    .IsUnicode(false)
-                    .HasComment("购方税号");
-
-                entity.Property(e => e.ChargeOff)
-                    .HasColumnType("money")
-                    .HasComment("销账金额");
-
-                entity.Property(e => e.DeliveryType).HasComment("配送方式");
-
-                entity.Property(e => e.Email)
-                    .HasMaxLength(50)
-                    .IsUnicode(false)
-                    .HasComment("联系人邮箱");
-
-                entity.Property(e => e.InvoiceCode)
-                    .IsRequired()
-                    .HasMaxLength(50)
-                    .IsUnicode(false)
-                    .HasComment("发票代码");
-
-                entity.Property(e => e.InvoiceDate)
-                    .HasColumnType("datetime")
-                    .HasComment("开票日期");
-
-                entity.Property(e => e.InvoiceMoney)
-                    .HasColumnType("money")
-                    .HasComment("开票金额");
-
-                entity.Property(e => e.InvoiceNo)
-                    .IsRequired()
-                    .HasMaxLength(50)
-                    .IsUnicode(false)
-                    .HasComment("发票号码");
-
-                entity.Property(e => e.InvoiceType).HasComment("发票类型");
-
-                entity.Property(e => e.LinkMan)
-                    .IsRequired()
-                    .HasMaxLength(50)
-                    .IsUnicode(false)
-                    .HasComment("联系人名称");
-
-                entity.Property(e => e.LinkManId).HasComment("联系人编号");
-
-                entity.Property(e => e.Memo)
-                    .HasMaxLength(1000)
-                    .IsUnicode(false)
-                    .HasComment("开票备注");
-
-                entity.Property(e => e.Mobile)
-                    .HasMaxLength(20)
-                    .IsUnicode(false)
-                    .HasComment("联系人手机号");
-
-                entity.Property(e => e.PbranchId)
-                    .HasColumnName("PBranchId")
-                    .HasComment("收票方，即上级公司/购方");
-
-                entity.Property(e => e.SaleAddress)
-                    .HasMaxLength(80)
-                    .IsUnicode(false)
-                    .HasComment("销方地址");
-
-                entity.Property(e => e.SaleBank)
-                    .HasMaxLength(200)
-                    .IsUnicode(false)
-                    .HasComment("销方开户行");
-
-                entity.Property(e => e.SaleBankAccount)
-                    .HasMaxLength(100)
-                    .IsUnicode(false)
-                    .HasComment("销方开户行账号");
-
-                entity.Property(e => e.SaleName)
-                    .HasMaxLength(200)
-                    .IsUnicode(false)
-                    .HasComment("销方名称");
-
-                entity.Property(e => e.SalePhone)
-                    .HasMaxLength(20)
-                    .IsUnicode(false)
-                    .HasComment("销方电话");
-
-                entity.Property(e => e.SaleTaxNo)
-                    .HasMaxLength(20)
-                    .IsUnicode(false)
-                    .HasComment("销方税号");
-
-                entity.Property(e => e.StatementId).HasDefaultValueSql("((0))");
-
-                entity.Property(e => e.UpdateTime)
-                    .HasColumnType("datetime")
-                    .HasComment("开票操作时间");
-
-                entity.Property(e => e.UserId).HasComment("开票操作人");
-            });
+           
 
             modelBuilder.Entity<DistributionInvoiceDetail>(entity =>
             {
