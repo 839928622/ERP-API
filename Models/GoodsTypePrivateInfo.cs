7b1e40f4338300fe007f4432a467c22121e8dd7f
Parker <jwk839928622@outlook.com>
2020-07-02 22:02:28 +0800

    add typeId back in

--- Models/GoodsTypePrivateInfo.cs
+++ Models/GoodsTypePrivateInfo.cs
@@ -4,8 +4,7 @@ namespace ERP_API.Models
 {
     public partial class GoodsTypePrivateInfo : BaseEntity
     {
-        public int Id { get; set; }
-    
+        public int TypeId { get; set; }
         public string Alias { get; set; }
         public int Sort { get; set; }
         public bool IsDisable { get; set; }
