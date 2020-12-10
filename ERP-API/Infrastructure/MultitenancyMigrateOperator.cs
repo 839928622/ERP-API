219f3365e8dbd6270ae7f84786df0ed29f3a6a12
Parker <jwk839928622@outlook.com>
2020-12-10 20:56:58 +0800

    comment out migrator

--- ERP-API/Infrastructure/MultitenancyMigrateOperator.cs
+++ ERP-API/Infrastructure/MultitenancyMigrateOperator.cs
@@ -12,32 +12,32 @@ namespace ERP_API.Infrastructure
 {
     public static class MultitenancyMigrateOperator
     {
-        public static void Migrate()
-        {
-            foreach (var databaseInfo in NeededToBeMigrateDatabaseList)
-            {
-                Mock<IHttpContextAccessor> httpContextAccessor = new Mock<IHttpContextAccessor>();
-                var httpContext = new Mock<HttpContext>();
-                httpContext.Setup(x => x.User.FindFirst(CustomizedClaims.BranchId)).Returns(new Claim(CustomizedClaims.BranchId, databaseInfo.BranchId));
-                //_accessor = new Mock<IHttpContextAccessor>();
-                //_accessor.Setup(x => x.HttpContext).Returns(httpContext);
-                httpContextAccessor.Setup(x => x.HttpContext).Returns(httpContext.Object);
+        //public static void Migrate()
+        //{
+        //    foreach (var databaseInfo in NeededToBeMigrateDatabaseList)
+        //    {
+        //        Mock<IHttpContextAccessor> httpContextAccessor = new Mock<IHttpContextAccessor>();
+        //        var httpContext = new Mock<HttpContext>();
+        //        httpContext.Setup(x => x.User.FindFirst(CustomizedClaims.BranchId)).Returns(new Claim(CustomizedClaims.BranchId, databaseInfo.BranchId));
+        //        //_accessor = new Mock<IHttpContextAccessor>();
+        //        //_accessor.Setup(x => x.HttpContext).Returns(httpContext);
+        //        httpContextAccessor.Setup(x => x.HttpContext).Returns(httpContext.Object);
 
 
-                using var applicationDbContext = new ApplicationDbContext(new DbContextOptions<ApplicationDbContext>(), httpContextAccessor.Object);
-                applicationDbContext.Database.Migrate();
-            }
+        //        using var applicationDbContext = new ApplicationDbContext(new DbContextOptions<ApplicationDbContext>(), httpContextAccessor.Object);
+        //        applicationDbContext.Database.Migrate();
+        //    }
             
-        }
+        //}
 
-        private static List<BranchDatabaseInfo> NeededToBeMigrateDatabaseList { get; set; }=new List<BranchDatabaseInfo>()
-        {
-            new BranchDatabaseInfo()
-            {
-                BranchName = "BBC",
-                BranchId = "1"
-            }
-        };
+        //private static List<BranchDatabaseInfo> NeededToBeMigrateDatabaseList { get; set; }=new List<BranchDatabaseInfo>()
+        //{
+        //    new BranchDatabaseInfo()
+        //    {
+        //        BranchName = "BBC",
+        //        BranchId = "1"
+        //    }
+        //};
 
         
     }
--- ERP-API/Startup.cs
+++ ERP-API/Startup.cs
@@ -114,7 +114,7 @@ namespace ERP_API
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            MultitenancyMigrateOperator.Migrate();
+            //MultitenancyMigrateOperator.Migrate();
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
